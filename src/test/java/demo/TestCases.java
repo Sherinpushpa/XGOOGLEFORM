bf40433632dff21c0c82a8204fb8b445723cd93c
Sherin Pushpa <sherinpushpabej1990@gmail.com>
2024-08-02 23:12:55 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,18 +12,19 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
 
+   Wrappers wrapper;
+
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
-
-     
+         
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
@@ -32,7 +34,7 @@ public class TestCases {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -48,6 +50,51 @@ public class TestCases {
 
         driver.manage().window().maximize();
     }
+
+    @Test
+    public void testCase01() throws InterruptedException {
+        wrapper = new Wrappers(driver);
+        wrapper.openUrl("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        Thread.sleep(2000);
+        wrapper.enterText(By.xpath("//div[@class='AgroKb']//input[@type='text']"), "Crio Learner");
+        Thread.sleep(1000);
+
+        wrapper.enterText(By.xpath("//textarea[@aria-label='Your answer']"), "I want to be the best QA Engineer! " + wrapper.epochTime());
+        Thread.sleep(1000);
+
+        wrapper.experienceRating(By.xpath("//div[contains(@class,'nWQGrd')]//div[@data-value='> 10']"));
+        Thread.sleep(1000);
+
+        wrapper.scrollToElement(By.xpath("//div[contains(@class,'nWQGrd')]//div[@data-value='> 10']"));
+        Thread.sleep(1000);
+
+        wrapper.clickElement(By.xpath("//div[@aria-label='Java']"));
+
+        wrapper.clickElement(By.xpath("//div[@aria-label='Selenium']"));
+
+        wrapper.clickElement(By.xpath("//div[@aria-label='TestNG']"));
+        Thread.sleep(1000);
+
+        wrapper.clickElement(By.xpath("(//div[@role='listbox'])[1]"));
+        Thread.sleep(1000);
+
+        wrapper.dropDown(By.xpath("//div[@role='option']/span"));
+        Thread.sleep(1000);
+
+        wrapper.enterText(By.xpath("//input[@type='date']"), wrapper.dateCalculator());
+        Thread.sleep(1000);
+
+        wrapper.enterText(By.xpath("//input[@aria-label='Hour']"), "07");
+        wrapper.enterText(By.xpath("//input[@aria-label='Minute']"), "30");
+
+        wrapper.clickElement(By.xpath("//span[text()='Submit']"));
+
+        wrapper.successMessage(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']"));
+        
+        Thread.sleep(5000);
+
+    }
+
 
     @AfterTest
     public void endTest()
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,12 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +19,66 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    WebDriver driver;
+
+    public Wrappers (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openUrl(String url) {
+        driver.get(url);
+    }
+
+    public void clickElement(By locator) {
+        WebElement element = driver.findElement(locator);
+        element.click();
+    }
+
+    public void enterText(By locator, String text) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+       // WebElement element = driver.findElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public long epochTime() {
+        long epoch = System.currentTimeMillis()/1000;
+        return epoch;
+    }
+
+    public void experienceRating(By locator) {
+        WebElement element = driver.findElement(locator);
+        element.click();
+    }
+
+    public void scrollToElement(By locator) {
+        WebElement element = driver.findElement(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void dropDown(By locator) {
+        List <WebElement> options = driver.findElements(locator);
+        System.out.println(options.size());
+        for (WebElement option : options) {
+            if (option.getText().equals("Mrs")) {
+                System.out.println(option.getText());
+                option.click();
+                break;
+            }
+        }
+    }
+
+    public String dateCalculator() {
+        LocalDateTime date7DaysAgo = LocalDateTime.now().minusDays(7);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = date7DaysAgo.format(format);
+        return formattedDate;
+    }
+
+    public void successMessage(By locator) {
+        WebElement element = driver.findElement(locator);
+        System.out.println("Successful message : " + element.getText());
+    }
+
 }
